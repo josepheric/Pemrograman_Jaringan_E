d9727dc99a9a09c21f7ae4aa9b6e41798c5e2361
josepheric <61129358+josepheric@users.noreply.github.com>
2021-06-17 20:29:55 +0700

    try multi process

--- progjar3/Tugas_3/multi_process.py
+++ progjar3/Tugas_3/multi_process.py
@@ -1,17 +1,22 @@
-from library import download_gambar, get_url_list
+from library import download_gambar, get_url_list, send_file
 import time
 import datetime
 from multiprocessing import Process
 
-def download_semua():
+IP_SERVER = "192.168.122.95"
+PORT = 5050
+
+def kirim_semua():
     texec = dict()
     urls = get_url_list()
     catat_awal = datetime.datetime.now()
     for k in urls:
+        download_gambar(urls[k],k)
         print(f"mendownload {urls[k]}")
         waktu = time.time()
         #bagian ini merupakan bagian yang mengistruksikan eksekusi fungsi download gambar secara multiprocess
-        texec[k] = Process(target=download_gambar, args=(urls[k],))
+        texec[k] = Process(target=kirim_gambar, args=(IP_SERVER,PORT,f"{k}.jpg"))
+        print('send to server succesfully')
         texec[k].start()
     #setelah menyelesaikan tugasnya, dikembalikan ke main process dengan join
     for k in urls:
@@ -21,4 +26,4 @@ def download_semua():
     print(f"Waktu TOTAL yang dibutuhkan {selesai} detik {catat_awal} s/d {catat_akhir}")
 #fungsi download_gambar akan dijalankan secara multi process
 if __name__=='__main__':
-    download_semua()
+    kirim_semua()
