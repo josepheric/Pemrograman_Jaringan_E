d7eb5ac757b20b24073b57250cb8981d1bfbdb15
josepheric <61129358+josepheric@users.noreply.github.com>
2021-06-17 20:50:13 +0700

    Update multi_thread_async.py

--- progjar3/Tugas_3/multi_thread_async.py
+++ progjar3/Tugas_3/multi_thread_async.py
@@ -1,22 +1,22 @@
-from library import download_gambar,get_url_list
+from library import download_gambar,get_url_list, send_file
 import time
 import datetime
 import concurrent.futures
 
+IP_SERVER = "192.168.122.244"
+PORT = 5050
 
-
-
-def download_semua():
+def kirim_semua():
     texec = dict()
     urls = get_url_list()
     status_task = dict()
     task = concurrent.futures.ThreadPoolExecutor(max_workers=4)
     catat_awal = datetime.datetime.now()
     for k in urls:
+        download_gambar(urls[k], k)
         print(f"mendownload {urls[k]}")
-        waktu = time.time()
-        #bagian ini merupakan bagian yang mengistruksikan eksekusi fungsi download gambar secara multithread
-        texec[k] = task.submit(download_gambar, urls[k])
+        texec[k] = task.submit(send_file,IP_SERVER, PORT, f"{k}.jpg")
+        print('send file succesfully')
 
     #setelah menyelesaikan tugasnya, dikembalikan ke main thread dengan memanggil result
     for k in urls:
@@ -32,4 +32,4 @@ def download_semua():
 #fungsi download_gambar akan dijalankan secara multithreading
 
 if __name__=='__main__':
-    download_semua()
+    kirim_semua()
